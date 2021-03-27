38e0271d64bbf0091bdd4d37e7da345f7b2ab481
tustoz <76657906+tustoz@users.noreply.github.com>
2021-03-27 22:05:03 +0700

    Added jwt and hashed password features

--- models/user.js
+++ models/user.js
@@ -1,25 +1,36 @@
 // import mongoose module
-const mongoose = require('mongoose')
+const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
 
 // create new mongoose models
 const userSchema = new mongoose.Schema({
   nama: {
     type: String,
-    required: true
+    required: true,
   },
   email: {
     type: String,
-    required: true
+    required: true,
   },
   nohp: {
     type: Number,
-    required: true
+    required: true,
   },
   jurusan: {
     type: String,
-    required: true
-  }
-})
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+});
+
+// hash user password before saving into database
+userSchema.pre("save", function (next) {
+  this.password = bcrypt.hashSync(this.password, 10);
+  next();
+});
 
 // export models
-module.exports = mongoose.model('User', userSchema)
+module.exports = mongoose.model("User", userSchema);
--- routes/users.routes.js
+++ routes/users.routes.js
@@ -1,51 +1,74 @@
+// import bcrypt and jwt module
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+
 // import and init .Router() method.
-const router = require('express').Router()
+const router = require("express").Router();
 
 // import mongodb models
-const User = require('../models/user')
+const User = require("../models/user");
 
 // REQUEST METHOD: GET, POST, PUT, PATCH, DELETE, HEAD
 
 // POST (CREATE)
-router.post('/', async (req, res) => {
+router.post("/", async (req, res) => {
   // create new user with user models
   const user = new User({
     nama: req.body.nama,
     email: req.body.email,
     nohp: req.body.nohp,
-    jurusan: req.body.jurusan
-  })
+    jurusan: req.body.jurusan,
+    password: req.body.password,
+  });
   // save user
   try {
-    const newUser = await user.save()
+    const newUser = await user.save();
     // if success, return statuscode 201 and display new user.
-    res.status(201).json(newUser)
+    res.status(201).json(newUser);
     // if error, catch error and return statuscode 400, with error message.
   } catch (err) {
-    res.status(400).json({ message: err.message })
+    res.status(400).json({ message: err.message });
   }
-})
+});
 
+// LOGIN AUTH
+router.post("/login", async (req, res) => {
+  const email = req.body.email;
+  const users = await User.findOne({ email: email });
+  if (users == null) {
+    return res.status(400).send("Cannot find users");
+  }
+  try {
+    if (await bcrypt.compare(req.body.password, users.password)) {
+      const accessToken = jwt.sign(email, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1h' });
+      res.json({ accessToken: accessToken }).send("Login Success");
+    } else {
+      res.send("Wrong passwords");
+    }
+  } catch {
+    res.status(500).send();
+  }
+});
 
 // GET ALL (READ)
 // Getting All user
-router.get('/', async (req, res) => {
+router.get("/", authenticateToken, async (req, res) => {
   try {
     // find user from database
-    const users = await User.find()
+    const users = await User.find(req.body.name);
     // display database
-    res.status(200).json(users)
+    res.status(200).json(users);
     // if error, catch error and display error message with json.
   } catch (err) {
-    res.status(500).json({ message: err.message })
+    res.status(500).json({ message: err.message });
   }
-})
+});
 
 // get users with pagination
 // localhost:3000/siswa?page=1&limit=10
 
 // GET (READ)
-router.get('/', async (req, res) => {
+router.get("/", async (req, res) => {
   try {
     // set limit number
     const limit = req.query.limit;
@@ -54,115 +77,132 @@ router.get('/', async (req, res) => {
     const page = parseInt(req.query.page);
 
     // set start index from zero
-    const startIndex = (page - 1) * limit
+    const startIndex = (page - 1) * limit;
 
     // set end index with page multiply by limit.
-    const endIndex = page * limit
+    const endIndex = page * limit;
 
     // create object
-    const results = {}
+    const results = {};
 
     // find user
-    const users = await User.find()
+    const users = await User.find();
 
     // if endIndex smaller than number of student, then return object named next with number of page and limit.
     if (endIndex < users.length) {
       results.next = {
         page: page + 1,
-        limit: limit
-      }
+        limit: limit,
+      };
     }
 
     // if startIndex bigger than number of student, then return object named previous with number of page and limit.
-    if (startIndex > 0){
+    if (startIndex > 0) {
       results.previous = {
         page: page - 1,
-        limit: limit
-      }
+        limit: limit,
+      };
     }
 
     // object named data is equals to number of slice between startIndex and endIndex.
-    results.data = users.slice(startIndex, endIndex)
+    results.data = users.slice(startIndex, endIndex);
 
     // if success return http statuscode 200 with json results.
-    res.status(200).json(results)
+    res.status(200).json(results);
     // if error, catch error, return http statuscode 500, and log error message.
   } catch (err) {
-    res.status(500).json({ message: err.message })
+    res.status(500).json({ message: err.message });
   }
-})
+});
 
 // Getting One User with Id
-router.get('/:id', getUser, (req, res) => {
-  res.json(res.user)
-})
+router.get("/:id", getUser, (req, res) => {
+  res.json(res.user);
+});
 
 // PATCH (UPDATE)
-router.patch('/:id', getUser, async (req, res) => {
+router.patch("/:id", getUser, async (req, res) => {
   // if body name value not null, set request user name to body name (replace old data).
   if (req.body.nama != null) {
-    res.user.nama = req.body.nama
+    res.user.nama = req.body.nama;
   }
   if (req.body.email != null) {
-    res.user.email = req.body.email
+    res.user.email = req.body.email;
   }
   if (req.body.nohp != null) {
-    res.user.nohp = req.body.nohp
+    res.user.nohp = req.body.nohp;
   }
   if (req.body.jurusan != null) {
-    res.user.jurusan = req.body.jurusan
+    res.user.jurusan = req.body.jurusan;
   }
   // update user from db
   try {
-    const updatedUser = await res.user.save()
+    const updatedUser = await res.user.save();
     // if success send http statuscode 200 with updated siswa.
-    res.status(200).json(updatedUser)
+    res.status(200).json(updatedUser);
     // if error catch error, and send http statuscode 400 with error message.
   } catch (err) {
-    res.status(400).json({ message: err.message })
+    res.status(400).json({ message: err.message });
   }
-})
+});
 
 // DELETE
-router.delete('/:id', getUser, async (req, res) => {
+router.delete("/:id", getUser, async (req, res) => {
   try {
     // delete user id from db
-    await res.user.remove()
-    const users = await User.find()
+    await res.user.remove();
+    const users = await User.find();
     // if success, send http statuscode 200 with message.
-    res.status(200).json({ message: 'Deleted Siswa', users})
+    res.status(200).json({ message: "Deleted Siswa", users });
     // if error catch error, and send http statuscode 500 with error message.
   } catch (err) {
-    res.status(500).json({ message: err.message })
+    res.status(500).json({ message: err.message });
   }
-})
+});
 
 // middleware -> promise -> request status
 async function getUser(req, res, next) {
-  let user
+  let user;
 
   // try search user by id with request params
   try {
-    user = await User.findById(req.params.id)
+    user = await User.findById(req.params.id);
     // if user not found, response with json statuscode 404, return message.
     if (user == null) {
-      return res.status(404).json({ message: `can't find data` })
+      return res.status(404).json({ message: `can't find data` });
     }
     // if error, catch error and response with json statuscode 500, return error message.
   } catch (err) {
-    return res.status(500).json({ message: err.message })
+    return res.status(500).json({ message: err.message });
   }
 
   // if user found, equals res.user with user.
-  res.user = user
-  next()
+  res.user = user;
+  next();
 }
 
+async function authenticateToken(req, res, next) {
+  const authHeader = req.headers["authorization"];
+  const token = authHeader && authHeader.split(" ")[1];
+  if (token == null) return res.sendStatus(401);
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, user) => {
+    if (err) return res.sendStatus(403);
+    req.user = user;
+    next();
+  });
+}
 // SwaggerUI API Docs
 
 /**
  * @swagger
  * components:
+ *   securitySchemes:
+ *     bearerAuth:
+ *       type: http
+ *       scheme: bearer
+ *       bearerFormat: JWT
+ *   security:
+ *     - bearerAuth: []
  *   schemas:
  *     Siswa:
  *       type: object
@@ -197,8 +237,31 @@ async function getUser(req, res, next) {
 
 /**
  * @swagger
+ * components:
+ *   schemas:
+ *     Login:
+ *       type: object
+ *       required:
+ *         - email
+ *         - password
+ *       properties:
+ *         email:
+ *           type: string
+ *           description: Email siswa
+ *         password:
+ *           type: string
+ *           description: Jurusan siswa
+ *       example:
+ *         email: maxi@filiasofia.sch.id
+ *         password: javascript
+ */
+
+/**
+ * @swagger
  * /v1/siswa:
  *   get:
+ *     security:
+ *       - bearerAuth: []
  *     summary: Get all student lists
  *     tags: [Siswa]
  *     responses:
@@ -222,6 +285,8 @@ async function getUser(req, res, next) {
  * @swagger
  * /v1/siswa/{id}:
  *   get:
+ *     security:
+ *       - bearerAuth: []
  *     summary: Get student by id
  *     tags: [Siswa]
  *     parameters:
@@ -253,6 +318,8 @@ async function getUser(req, res, next) {
  * @swagger
  * /v1/siswa?page={page}&limit={limit}:
  *   get:
+ *     security:
+ *       - bearerAuth: []
  *     summary: Get student list with pagination
  *     tags: [Siswa]
  *     parameters:
@@ -309,8 +376,40 @@ async function getUser(req, res, next) {
 
 /**
  * @swagger
+ * /v1/siswa/login:
+ *   post:
+ *     summary: Login auth
+ *     tags: [Siswa]
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             $ref: '#/components/schemas/Login'
+ *     responses:
+ *       200:
+ *         description: users was successfully authenticate
+ *       201:
+ *         description: The users was successfully created
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/Login'
+ *       401:
+ *         description: Unauthorized
+ *       403:
+ *         description: Forbidden
+ *       500:
+ *         description: Some server error
+ *
+ */
+
+/**
+ * @swagger
  * /v1/siswa/{id}:
  *  put:
+ *    security:
+ *      - bearerAuth: []
  *    summary: Update student by the id
  *    tags: [Siswa]
  *    parameters:
@@ -343,6 +442,8 @@ async function getUser(req, res, next) {
  * @swagger
  * /v1/siswa/{id}:
  *   delete:
+ *     security:
+ *       - bearerAuth: []
  *     summary: Remove student by id
  *     tags: [Siswa]
  *     parameters:
@@ -360,5 +461,4 @@ async function getUser(req, res, next) {
  *         description: The user was not found
  */
 
-
-module.exports = router
+module.exports = router;
